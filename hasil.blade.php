e5a82d11c0b6fae2ff6464dc00017225b568a593
hadiha <hadi.h115@gmail.com>
2022-03-20 00:01:44 +0700

    Add files via upload

---
+++ hasil.blade.php
@@ -0,0 +1,76 @@
+
+<div class="container">
+    <div class="col-12">
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <td colspan="2">Data Warga Penerima Bantuan</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th width="30%">Nama</th>
+                    <td>{{ $record['nama'] }}</td>
+                </tr>
+                <tr>
+                    <th>NIK</th>
+                    <td>{{ $record['nik'] }}</td>
+                </tr>
+                <tr>
+                    <th>Nomor KK</th>
+                    <td>{{ $record['no_kk'] }}</td>
+                </tr>
+                <tr>
+                    <th>Foto KTP</th>
+                    <td>{{ $record['ktp'] ?? '-' }}</td>
+                </tr>
+                <tr>
+                    <th>Foto KK</th>
+                    <td>{{ $record['kk'] ?? '-'}}</td>
+                </tr>
+                <tr>
+                    <th>Umur</th>
+                    <td>{{ $record['umur'] }}</td>
+                </tr>
+                <tr>
+                    <th>Jenis Kelamin</th>
+                    <td>{{ $record['jk'] }}</td>
+                </tr>
+                <tr>
+                    <th>Provinsi</th>
+                    <td>{{ $record['provinsi'] }}</td>
+                </tr>
+                <tr>
+                    <th>Kota/ Kab</th>
+                    <td>{{ $record['kota'] }}</td>
+                </tr>
+                <tr>
+                    <th>Kecamatan</th>
+                    <td>{{ $record['camat'] }}</td>
+                </tr>
+                <tr>
+                    <th>Kelurahan</th>
+                    <td>{{ $record['lurah'] }}</td>
+                </tr>
+                <tr>
+                    <th>Alamat</th>
+                    <td>{{ $record['alamat'] }}, Rt. {{ $record['rt'] }} - Rw. {{ $record['rw'] }}</td>
+                </tr>
+                <tr>
+                    <th>Pengahsilan Sebelum Pandemin</th>
+                    <td>Rp. {{ number_format($record['sebelum']) }}</td>
+                </tr>
+                <tr>
+                    <th>Pengahsilan Sesudah Pandemin</th>
+                    <td>Rp. {{ number_format($record['sesudah']) }}</td>
+                </tr>
+                <tr>
+                    <th>Alasan Membutuhkan Bantuan</th>
+                    <td>{{ $record['alasan'] }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+
+
